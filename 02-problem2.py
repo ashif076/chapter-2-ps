db63326d91f7ab12c669ea9933b1544252ef003b
ashif076 <ashifsheikh138@gmail.com>
2026-05-30 20:16:23 +0530

    Refactor remainder calculation and improve output clarity (commited by Arbaz)

--- 02-problem2.py
+++ 02-problem2.py
@@ -1,5 +1,6 @@
 #write a python program to find remainder when a number is divided by z.
 a   = 34
 b = 5
-
+c = a%b
 print("remainder when a is divided by b is", a%b)
+print("remainder when a is divided by b is", c)
